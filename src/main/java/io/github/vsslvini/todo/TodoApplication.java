86513e14f1da880264ee0729aa3050f58783b08c
vsslvini <vsslviniciussousa@gmail.com>
2024-06-21 14:51:11 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/io/github/vsslvini/todo/TodoApplication.java
@@ -0,0 +1,21 @@
+package io.github.vsslvini.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodoApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
